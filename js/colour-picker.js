0240170bbb896e931f103209618460fb9951493a
unsettledgames <47360416+unsettledgames@users.noreply.github.com>
2021-03-05 09:45:44 +0100

    Uploaded project

---
+++ js/colour-picker.js
@@ -0,0 +1,19 @@
+/**utilities**/
+//=include utilities/on.js
+//=include utilities/onChildren.js
+//=include utilities/onClick.js
+//=include utilities/onClickChildren.js
+//=include utilities/select.js
+//=include utilities/getSetText.js
+//=include utilities/getSetValue.js
+//=include utilities/hexToRgb.js
+//=include utilities/rgbToHex.js
+//=include utilities/rgbToHsl.js
+//=include utilities/hslToRgb.js
+//=include libraries/cookies.js
+//=include _pixelEditorUtility.js
+
+
+/**functions**/
+//=include _algorithms.js
+//=include _colorPicker.js
